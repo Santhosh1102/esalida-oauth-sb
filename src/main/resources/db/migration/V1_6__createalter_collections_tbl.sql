0cfab2c65cdc1a59ac851fcf51b4a255eef8c5e7
Santhoshkumar Aagiru <santhoshkumar_aagiru@tecnics.com>
2017-12-19 16:16:04 +0530

    Added migration script for shared collection

--- src/main/resources/db/migration/V1_6__createalter_collections_tbl.sql
+++ src/main/resources/db/migration/V1_6__createalter_collections_tbl.sql
@@ -1,3 +1,26 @@
+DROP TABLE `estecnicslabscom`.`collectioninfo`;
+DROP TABLE `estecnicslabscom`.`collections`;
+
+CREATE TABLE `estecnicslabscom`.`collections` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `userId` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_collections` (`name`,`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `estecnicslabscom`.`collectioninfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `docId` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `collectionId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_collectioninfo` (`docId`,`collectionId`),
+  KEY `fk_collectioninfo_1_idx` (`collectionId`),
+  CONSTRAINT `fk_collectioninfo_1` FOREIGN KEY (`collectionId`) REFERENCES `collections` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 CREATE TABLE `estecnicslabscom`.`sharedCollections` (
  `id` INT(11) NOT NULL AUTO_INCREMENT,
  `collectionId` INT(11) NOT NULL,
@@ -10,28 +33,3 @@ CREATE TABLE `estecnicslabscom`.`sharedCollections` (
  REFERENCES `estecnicslabscom`.`collections` (`id`)
  ON DELETE NO ACTION
  ON UPDATE NO ACTION);
-
-DROP TABLE `estecnicslabscom`.`collectioninfo`;
-
-DROP TABLE `estecnicslabscom`.`collections`;
-
-CREATE TABLE `collections` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  `userId` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uq_collections` (`name`,`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE `collectioninfo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `docId` varchar(45) DEFAULT NULL,
-  `type` varchar(45) DEFAULT NULL,
-  `collectionId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uq_collectioninfo` (`docId`,`collectionId`),
-  KEY `fk_collectioninfo_1_idx` (`collectionId`),
-  CONSTRAINT `fk_collectioninfo_1` FOREIGN KEY (`collectionId`) REFERENCES `collections` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=112 DEFAULT CHARSET=latin1;
-
