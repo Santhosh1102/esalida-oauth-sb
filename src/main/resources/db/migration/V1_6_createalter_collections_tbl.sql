7197ef1a10096e8782b0bf8236046dcb3b7aeab6
Sandeep <sandeep_gandham@tecnics.com>
2017-12-19 15:56:39 +0530

    Dropped the existing tables and creating them again

--- src/main/resources/db/migration/V1_6_createalter_collections_tbl.sql
+++ src/main/resources/db/migration/V1_6_createalter_collections_tbl.sql
@@ -11,14 +11,27 @@ CREATE TABLE `estecnicslabscom`.`sharedCollections` (
  ON DELETE NO ACTION
  ON UPDATE NO ACTION);
 
-ALTER TABLE `estecnicslabscom`.`collectioninfo`
-ADD CONSTRAINT `fk_collectioninfo_1`
-  FOREIGN KEY (`collectionId`)
-  REFERENCES `estecnicslabscom`.`collections` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+DROP TABLE `estecnicslabscom`.`collectioninfo`;
 
-ALTER TABLE estecnicslabscom.collectioninfo ADD CONSTRAINT uq_collectioninfo UNIQUE(docId, collectionId);
+DROP TABLE `estecnicslabscom`.`collections`;
 
-ALTER TABLE estecnicslabscom.collections ADD CONSTRAINT uq_collections UNIQUE(name, userId);
+CREATE TABLE `collections` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `userId` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_collections` (`name`,`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `collectioninfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `docId` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `collectionId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_collectioninfo` (`docId`,`collectionId`),
+  KEY `fk_collectioninfo_1_idx` (`collectionId`),
+  CONSTRAINT `fk_collectioninfo_1` FOREIGN KEY (`collectionId`) REFERENCES `collections` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=112 DEFAULT CHARSET=latin1;
 
