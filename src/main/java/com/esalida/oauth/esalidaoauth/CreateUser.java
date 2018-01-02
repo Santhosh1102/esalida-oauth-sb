2791f2989c3134e23d2643142bba526473fe8ccf
Santhoshkumar Aagiru <santhoshkumar_aagiru@tecnics.com>
2018-01-02 14:20:52 +0530

    changed cmdrunner to accept 0 args

--- src/main/java/com/esalida/oauth/esalidaoauth/CreateUser.java
+++ src/main/java/com/esalida/oauth/esalidaoauth/CreateUser.java
@@ -1,4 +1,4 @@
-package com.esalida.oauth.esalidaoauth.utils;
+package com.esalida.oauth.esalidaoauth;
 
 import com.esalida.oauth.esalidaoauth.models.Employee;
 import com.esalida.oauth.esalidaoauth.models.User;
@@ -9,6 +9,8 @@ import com.esalida.oauth.esalidaoauth.repositories.UserProfileRepository;
 import com.esalida.oauth.esalidaoauth.repositories.UserRepository;
 import com.esalida.oauth.esalidaoauth.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -16,12 +18,14 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by tecnicsdev on 1/1/18.
  */
 @Component
-public class CreateUser implements CommandLineRunner {
+public class CreateUser implements ApplicationRunner {
 
     @Autowired
     UserRepository userRepository;
@@ -32,48 +36,6 @@ public class CreateUser implements CommandLineRunner {
     @Autowired
     UserProfileRepository userProfileRepository;
 
-
-    @Override
-    public void run(String... strings) throws Exception {
-        if(strings.length>0){
-            String csvFile=strings[0];
-            Long tenantId=new Long(strings[1]);
-
-            BufferedReader br = null;
-            String line = "";
-            String cvsSplitBy = ",";
-
-            try {
-
-                br = new BufferedReader(new FileReader(csvFile));
-                while ((line = br.readLine()) != null) {
-                    // use comma as separator
-                    String[] userData = line.split(cvsSplitBy);
-
-                    Employee employee=new Employee();
-                    employee.setUserId(new Long(userData[0]));
-                    employee.setFirstName(userData[1]);
-                    employee.setLastName(userData[2]);
-                    employee.setEmail(userData[1]+userData[2]+"@enron.com");
-                    employee.setUserName(userData[1]+userData[2]);
-                    saveEmployee(employee, tenantId);
-                }
-                System.out.println("Saved all users successfully");
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (br != null) {
-                    try {
-                        br.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-    }
 
     private UserProfile saveEmployee(Employee employee, Long tenantId){
         String email = employee.getEmail();
@@ -110,5 +72,52 @@ public class CreateUser implements CommandLineRunner {
             return savedUserProfile;
         }
         return null;
+    }
+
+    @Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+
+        List<String> fileLocs = applicationArguments.getOptionValues("fileLoc");
+        List<String> tenantIds = applicationArguments.getOptionValues("tenantId");
+        if(fileLocs!=null && tenantIds!=null){
+            String csvFile=fileLocs.get(0);
+            Long tenantId=new Long(tenantIds.get(0));
+            System.out.println(csvFile);
+            System.out.println(tenantId);
+            BufferedReader br = null;
+            String line = "";
+            String cvsSplitBy = ",";
+
+            try {
+
+                br = new BufferedReader(new FileReader(csvFile));
+                while ((line = br.readLine()) != null) {
+                    // use comma as separator
+                    String[] userData = line.split(cvsSplitBy);
+
+                    Employee employee=new Employee();
+                    employee.setUserId(new Long(userData[0]));
+                    employee.setFirstName(userData[1]);
+                    employee.setLastName(userData[2]);
+                    employee.setEmail(userData[1]+userData[2]+"@enron.com");
+                    employee.setUserName(userData[1]+userData[2]);
+                    saveEmployee(employee, tenantId);
+                }
+                System.out.println("Saved all users successfully");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
     }
 }
